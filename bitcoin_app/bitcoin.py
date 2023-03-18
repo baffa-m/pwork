7983e2e4031e4da09719bba7aa72ac381d8423d3
Ahmad Almustapha <ahmadalmustapha4@gmail.com>
2023-03-18 13:31:55 +0100

    initial commit

---
+++ bitcoin_app/bitcoin.py
@@ -0,0 +1,131 @@
+from fastapi import FastAPI, Depends, Response
+from sqlalchemy import create_engine, Column, Integer, DateTime, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+import requests
+import io
+import base64
+from fastapi.responses import HTMLResponse
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:some-password@127.0.0.1:3306/my-db"
+
+# Create a SQLAlchemy engine for the MySQL database
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:toor@172.17.0.2:3306/leads-db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a Session class for interacting with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db = SessionLocal()
+Base = declarative_base()
+
+class Price(Base):
+    __tablename__ = 'prices'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    price = Column(Float)
+    timestamp = Column(DateTime, default=datetime.utcnow())
+    
+    
+class PriceBase(BaseModel):
+    price: float
+    timestamp: datetime
+    
+    
+class PriceInDB(PriceBase):
+    id: int
+    
+    
+Base.metadata.create_all(engine)
+
+    
+    
+url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    price = data['data']['amount']
+    
+    new_price = Price(price=price, timestamp=datetime.utcnow())
+    db.add(new_price)
+    db.commit()
+
+
+
+
+
+# Fetch the prices from the database
+prices = db.query(Price).order_by(Price.timestamp).all()
+
+# Extract the timestamps and prices from the database
+timestamps = [price.timestamp for price in prices]
+prices = [price.price for price in prices]
+
+# Define the time range for the graph (e.g., last 24 hours)
+end_time = datetime.utcnow()
+start_time = end_time - timedelta(hours=24)
+
+# Filter the prices to only include those within the time range
+timestamps_filtered = []
+prices_filtered = []
+for i in range(len(timestamps)):
+    if timestamps[i] >= start_time and timestamps[i] <= end_time:
+        timestamps_filtered.append(timestamps[i])
+        prices_filtered.append(prices[i])
+
+
+    
+
+app = FastAPI()
+
+
+@app.get("/price")
+async def get_price():
+    latest_price = db.query(Price).filter(Price.id).first()
+    if latest_price:
+        return latest_price
+    else:
+        return {"error": "Failed to fetch Bitcoin-USD price from database"}
+
+
+
+
+# generate the plot
+
+@app.get("/plot", response_class=HTMLResponse)
+async def plot_graph():
+    # Generate the plot
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(timestamps_filtered, prices_filtered)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Price (USD)')
+    ax.set_title('Bitcoin-USD Price over Time')
+    
+    # Render the plot to a PNG image
+    canvas = FigureCanvas(fig)
+    png_output = io.BytesIO()
+    canvas.print_png(png_output)
+    
+    # Convert the PNG image to base64 encoding
+    png_output.seek(0)
+    b64_output = base64.b64encode(png_output.read()).decode("utf-8")
+    
+    # Render the HTML to displays the graph
+    html_output = f"""
+    <html>
+        <body>
+            <h1>Bitcoin-USD Price over Time</h1>
+            <img src="data:image/png;base64,{b64_output}">
+        </body>
+    </html>
+    """
+    return html_output
---
+++ leads/main.py
@@ -0,0 +1,110 @@
+from fastapi import FastAPI
+import requests
+import json
+from typing import List
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = FastAPI()
+
+Base = declarative_base()
+
+class Lead(BaseModel):
+    phone_work: str
+    first_name: str
+    last_name: str
+
+class LeadsResponse(BaseModel):
+    leads: List[Lead]
+
+class ErrorResponse(BaseModel):
+    message: str
+
+
+class LeadModel(Base):
+    __tablename__ = "leads"
+    id = Column(Integer, primary_key=True, index=True)
+    phone_work = Column(String(255))
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:toor@172.17.0.2:3306/leads-db"
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./leads.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  
+    
+    
+    
+@app.get("/get_leads", responses={404: {"model": ErrorResponse}})
+async def get_leads():
+    # Authenticate with SuiteCRM and get session ID
+    url = "https://suitecrmdemo.dtbc.eu/service/v4/rest.php"
+
+    def restRequest(method, arguments):
+        post = {
+            "method": method,
+            "input_type": "JSON",
+            "response_type": "JSON",
+            "rest_data": json.dumps(arguments)
+        }
+        response = requests.post(url, data=post)
+        result = json.loads(response.text)
+        return result
+
+    userAuth = {
+        "user_name": "Demo",
+        "password": "f0258b6685684c113bad94d91b8fa02a"
+    }
+    appName = "My SuiteCRM REST Client"
+    nameValueList = []
+
+    args = {
+        "user_auth": userAuth,
+        "application_name": appName,
+        "name_value_list": nameValueList
+    }
+
+    result = restRequest("login", args)
+    session_id = result["id"]
+    
+    
+    
+    # Fetch leads from SuiteCRM
+    entry = {
+        "session": session_id,
+        "module_name": "Leads",
+        "query": "",
+        "order_by": "",
+        "offset": "",
+        "select_fields": ["phone_work", "first_name", "last_name"],
+        "link_name_to_fields_array": "",
+        "max_results": 200,
+        "deleted": False,
+        "favorites": False
+    }
+    result = restRequest("get_entry_list", entry)
+    if not result:
+        return ErrorResponse(message="Failed to fetch leads from SuiteCRM.")
+    leads = result["entry_list"]
+    
+    # Store leads in MySQL
+    db = SessionLocal()
+    for lead in leads:
+        phone_work = lead["name_value_list"]["phone_work"]["value"]
+        first_name = lead["name_value_list"]["first_name"]["value"]
+        last_name = lead["name_value_list"]["last_name"]["value"]
+        db_lead = LeadModel(phone_work=phone_work, first_name=first_name, last_name=last_name)
+        existing_lead = db.query(LeadModel).filter_by(phone_work=db_lead.phone_work).first()
+        if not existing_lead:
+            db.add(db_lead)
+    db.commit()
+    
+    # Return leads as response
+    return LeadsResponse(leads=[Lead(phone_work=lead.phone_work, first_name=lead.first_name, last_name=lead.last_name) for lead in db.query(LeadModel).all()])
